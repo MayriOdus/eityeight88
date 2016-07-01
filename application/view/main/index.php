fb7ee529600781fae0181737b800d077d592107a
OM <oom34299@gmail.com>
2016-07-01 12:51:49 +0700

    commit last broken

--- application/view/main/index.php
+++ application/view/main/index.php
@@ -1,14 +1,14 @@
-<div class="row black-row">
+<div class="row">
 	
 	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
+		<div class="col-md-8 col-md-offset-2">
 			<div id="carousel-top" class="carousel slide" data-ride="carousel" data-interval="15000">	
 				<!-- Wrapper for slides -->
 				<div class="carousel-inner" role="listbox">            
 
 					<div class="item active">
-						<div align="center">
-							  <video autoplay loop muted style="width:100%;">
+						<div align="center" class="embed-responsive embed-responsive-16by9">
+							  <video autoplay loop muted class="embed-responsive-item">
 								 <source src="<?=URL;?>video/01.mp4" type="video/mp4">
 								 Your browser does not support the video tag.
 							  </video>
@@ -16,8 +16,8 @@
 					</div> 
 
 					<div class="item">
-						<div align="center">
-							  <video autoplay loop muted style="width:100%;">
+						<div align="center" class="embed-responsive embed-responsive-16by9">
+							  <video autoplay loop muted style="width:100%;" class="embed-responsive-item">
 								 <source src="<?=URL;?>video/02.mp4" type="video/mp4" onclick="this.play();">
 								 Your browser does not support the video tag.
 							  </video>
@@ -25,8 +25,8 @@
 					</div>
 
 					<div class="item">
-						<div align="center" style="width:100%;">
-							  <input type="image" src="<?=URL;?>img/88_main01.gif">
+						<div align="center" class="img-slide-res">
+							  <img class="img-responsive" src="<?=URL;?>img/88_main01.gif">
 						</div>
 					</div>
 				</div>
@@ -59,10 +59,112 @@
 <div class="row">
 	
 	<div class="col-md-12 text-center">
-		<img src="<?=URL;?>img/AW-88-Button-Wed-01.gif" style="width:130px;margin-right:10px;">
-		<img src="<?=URL;?>img/AW-88-Button-Wed-02.gif" style="width:130px;margin-right:10px;">
-		<img src="<?=URL;?>img/AW-88-Button-Wed-03.gif" style="width:130px;margin-right:10px;">
-		<img src="<?=URL;?>img/AW-88-Button-Wed-04.gif" style="width:130px;margin-right:10px;">
+		<img src="<?=URL;?>img/AW-88-Button-Wed-01.gif" style="width:100px;margin-right:10px;">
+		<img src="<?=URL;?>img/AW-88-Button-Wed-02.gif" style="width:100px;margin-right:10px;">
+		<img src="<?=URL;?>img/AW-88-Button-Wed-03.gif" style="width:100px;margin-right:10px;">
+		<img src="<?=URL;?>img/AW-88-Button-Wed-04.gif" style="width:100px;margin-right:10px;">
 	</div>
 
 </div>
+
+<br>
+
+<div class="row sep-line"></div>	
+
+<br>
+
+<div class="row">
+
+	<div class="col-sm-4 col-lg-4 col-md-4">
+		<div class="thumbnail">
+			<img src="<?=URL;?>img/Gif-Cushion.gif" alt="">
+			<div class="caption text-center">
+				<h4><a href="#">VER. 88 BOUNCE UP PACT</a></h4>
+				<h4>฿490</h4>
+				<p>Extended Coverage, Intended Closeness A foundation used nationwide... <a target="_blank" href="#"> More </a>.</p>
+			</div>
+			<!-- <div class="ratings">
+				<p class="pull-right">15 reviews</p>
+				<p>
+					<span class="glyphicon glyphicon-star"></span>
+					<span class="glyphicon glyphicon-star"></span>
+					<span class="glyphicon glyphicon-star"></span>
+					<span class="glyphicon glyphicon-star"></span>
+					<span class="glyphicon glyphicon-star"></span>
+				</p>
+			</div> -->
+		</div>
+	</div>
+
+	<div class="col-sm-4 col-lg-4 col-md-4">
+		<div class="thumbnail">
+			<img src="<?=URL;?>img/Gif-Dewy-2.gif" alt="">
+			<div class="caption text-center">
+				<h4><a href="#">EITY EIGHT DEWY FACE GLOW</a></h4>
+				<h4>฿650</h4>
+				<p>soft primer applied prior to foundation that provides you a new dewy radiance finish... <a target="_blank" href="#"> More </a>.</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="col-sm-4 col-lg-4 col-md-4">
+		<div class="thumbnail">
+			<img src="<?=URL;?>img/Gif-Lips-Box.gif" alt="">
+			<div class="caption text-center">
+				<h4><a href="#">EITY EIGHT DEWY FACE GLOW</a></h4>
+				<h4>฿990</h4>
+				<p>NO.21 LIGHT BEIGE + CURVED FACE BRUSH . <a target="_blank" href="#"> More </a>.</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="col-sm-4 col-lg-4 col-md-4">
+		<div class="thumbnail">
+			<img src="<?=URL;?>img/Gif-Liquid-Foundation-and-Brush.gif" alt="">
+			<div class="caption text-center">
+				<h4><a href="#">HOLIDAY LIP PENCIL SET</a></h4>
+				<h4>฿990</h4>
+				<p>MADE IN KORE. <a target="_blank" href="#"> More </a>.</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="col-sm-4 col-lg-4 col-md-4">
+		<div class="thumbnail">
+			<img src="<?=URL;?>img/Gif-Mirror.gif" alt="">
+			<div class="caption text-center">
+				<h4><a href="#">MINI MIRROR ORANGE NEON</a></h4>
+				<h4> N/A </h4>
+				<p>NOT FOR SALE <a target="_blank" href="#"> More </a>.</p>
+			</div>
+		</div>
+	</div>
+
+
+</div>
+
+
+<br>
+
+<div class="row sep-line"></div>	
+
+<!-- Footer -->
+<footer class="footer footer-basic-centered">
+
+		<p class="footer-company-motto">Eityeight88</p>
+		
+		<p class="footer-links">
+			<ul class="social-network social-circle ">
+				<li><a href="#" class="icoRss" title="Rss"><i class="fa fa-rss"></i></a></li>
+				<li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+				<li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+				<li><a href="#" class="icoGoogle" title="Google +"><i class="fa fa-google-plus"></i></a></li>
+				<li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+			</ul>
+		</p>
+
+		<p class="footer-company-name">
+			© <script>document.write(new Date().getFullYear())</script>, EITYEIGHT88. ALL RIGHTS RESERVED.
+		</p>
+
+</footer><!--End footer 2-->
