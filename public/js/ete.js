33aefb29c9db2532a41ca0dc631ad822f6f91827
ompakap <oom34299@gmail.com>
2016-07-08 02:24:43 +0700

    fixed ete.js

--- public/js/ete.js
+++ public/js/ete.js
@@ -326,10 +326,9 @@ function debounce(func, wait, immediate)
     };
 };
 
-
 var tag = document.createElement('script');
-
 tag.src = "//www.youtube.com/iframe_api";
+
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
