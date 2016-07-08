409cd71e655734a10e5898c86df21955de80b924
OM <oom34299@gmail.com>
2016-07-08 18:05:19 +0700

    fixing adminp

--- public/adminp/headhtml.php
+++ public/adminp/headhtml.php
@@ -1,30 +1,30 @@
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="canonical" href="http://www.eityeight.com/" />
-	<meta name="description" content="ค้นพบผลิตภัณฑ์เครื่องสำอางมากมายได้ที่นี่" />
-	<meta name="description" content="Shop makeup and skincare products on eityeight Cosmetics online. Learn&#039;s latest looks, makeup tips and techniques" />
-	<meta name="keywords" content="Cosmetics,makeup,tip makeup" />
-	<meta name="author" content="infinity" />
-	<meta charset="UTF-8">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="canonical" href="http://www.eityeight.com/" />
+<meta name="description" content="ค้นพบผลิตภัณฑ์เครื่องสำอางมากมายได้ที่นี่" />
+<meta name="description" content="Shop makeup and skincare products on eityeight Cosmetics online. Learn&#039;s latest looks, makeup tips and techniques" />
+<meta name="keywords" content="Cosmetics,makeup,tip makeup" />
+<meta name="author" content="infinity" />
+<meta charset="UTF-8">
 
-    <title>eityeight.com</title>
+<title>eityeight.com</title>
 
-     <!-- Bootstrap -->
-	<link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css">
-	<link href="../css/jquery-ui.css" rel="stylesheet" type="text/css">
-	<link href="../css/style.css" rel="stylesheet" type="text/css">
+ <!-- Bootstrap -->
+<link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css">
+<link href="../css/jquery-ui.css" rel="stylesheet" type="text/css">
+<link href="../css/style.css" rel="stylesheet" type="text/css">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-     <!--[if lt IE 9]>
-      <script src="../js_admin/js/html5shiv.js"></script>
-      <script src="../js_admin/js/respond.min.js"></script>
-    <![endif]--> 
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+ <!--[if lt IE 9]>
+  <script src="../js_admin/js/html5shiv.js"></script>
+  <script src="../js_admin/js/respond.min.js"></script>
+<![endif]--> 
 
-	<script src="../js_admin/jquery-1.10.2.js"></script>
+<script src="../js/jquery-1.12.0.min.js"></script>
 
-	<script src="../js_admin/bootstrap.js"></script>
-	<script src="../js_admin/holder.js"></script>
-	<script src="../js_admin/bootstrap-modal.js"></script>
-	<script src="../js_admin/jquery-ui.js"></script>
+<script src="../js_admin/bootstrap.js"></script>
+<!-- <script src="../js_admin/holder.js"></script> -->
+<script src="../js_admin/bootstrap-modal.js"></script>
+<script src="../js_admin/jquery-ui.min.js"></script>
