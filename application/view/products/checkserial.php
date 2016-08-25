8c756ab66270a3406107d59d2d89ab59d6604e08
ompakap <oom34299@gmail.com>
2016-08-25 23:12:01 +0700

    after

--- application/view/products/checkserial.php
+++ application/view/products/checkserial.php
@@ -1,25 +1,44 @@
 
 
 <div class="row item-product-margin">
-    <div class="col-md-6 col-md-offset-3 col-lg-6 col-lg-offset-3 col-sm-6 col-sm-offset-3">
-        <form action="p_login.php" method="post" class="contact-form">
-            <fieldset>
+    <div class="col-md-12">
 
-                <div class="contact">
-                    <div class="hd text-uppercase"><?=_CHECK_SERIAL;?></div>
-                </div>
+		<div class="row">
+			
+			<div class="col-sm-6 col-md-6" style="font-size:40px;">
+				SLIDE
+			</div>
 
-                <div class="form-group">
-                    <input type="text" name="input-serial" id="input-serial" class="form-control" placeholder="<?=_SERIAL_NUMBER;?>">
-                </div>
-               
-                <div class="row">
-                    <div class="col-xs-12">
-                        <input type="button" class="btn btn-black btn-block sharp" id="btn-chkall-serial" value="<?=_VALIDATE;?>">
-                    </div>
-                </div>
-            </fieldset>
-        </form>
+			<div class="col-sm-6 col-md-6">
+				<form action="p_login.php" method="post" class="contact-form">
+					<fieldset>
+
+						<div class="contact">
+							<div class="hd text-uppercase"><?=_CHECK_SERIAL;?></div>
+						</div>
+
+						<div class="form-group">
+							<input type="text" name="input-serial" id="input-serial" class="form-control" placeholder="<?=_SERIAL_NUMBER;?>">
+						</div>
+					   
+						<div class="row">
+							<div class="col-xs-12">
+								<input type="button" class="btn btn-black btn-block sharp" id="btn-chkall-serial" value="<?=_VALIDATE;?>">
+							</div>
+						</div>
+					</fieldset>
+				</form>
+			</div>
+
+		</div>	
+		
+		<br><br>
+		<div class="row">
+			<div class="col-sm-12 text-center" style="font-size:40px;">
+				DETAIL HOW TO CHECKING
+			</div>
+		</div>
+
     </div>
 
 
