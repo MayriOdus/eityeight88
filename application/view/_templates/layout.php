a1b907255b565dd7bc15beeb521ccbc3adc6dccb
OM <oom34299@gmail.com>
2017-06-20 13:36:39 +0700

    add trademark

--- application/view/_templates/layout.php
+++ application/view/_templates/layout.php
@@ -51,7 +51,9 @@
   </div>
 </div>
 
-<script src="https://www.trustmarkthai.com/callbackData/initialize.js?t=a6f39d1-17-5-f702dc277e3334381fe25c312a28457fa1d22e" id="dbd-init"></script><div id="Certificate-banners"></div>
+<center>
+	<script src="https://www.trustmarkthai.com/callbackData/initialize.js?t=a6f39d1-17-5-f702dc277e3334381fe25c312a28457fa1d22e" id="dbd-init"></script><div id="Certificate-banners"></div>
+</center>
 
 <?php require APP . 'view/_templates/script.php'; ?>
 
